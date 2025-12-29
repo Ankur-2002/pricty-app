46f4e5a9ea40566c64f510644a2431108238aa1a
Ankur2002 <ankurchauraisa002@gmail.com>
2025-12-30 00:46:53 +0530

    Carousel Added

--- app/page.tsx
+++ app/page.tsx
@@ -19,18 +19,6 @@ import Block from '../images/Intregration.png';
 import Integration from '../images/Block.png';
 import TickSvg from '@/components/ui/TickSvg';
 import AnimatedCarousel from '@/components/ui/animated-carousel';
-import Caro1 from '../images/Caro1.png';
-import Caro2 from '../images/Caro2.png';
-import Caro3 from '../images/Caro3.png';
-import Caro4 from '../images/Caro4.png';
-import Caro5 from '../images/Caro5.png';
-import Caro6 from '../images/Caro6.png';
-import Caro7 from '../images/Caro7.png';
-import Caro8 from '../images/Caro8.png';
-import Caro9 from '../images/Caro9.png';
-import Caro10 from '../images/Caro10.png';
-import Caro11 from '../images/Caro11.png';
-import Caro12 from '../images/Caro12.png';
 
 export default function Home() {
   const [isVisible, setIsVisible] = useState(false);
